2dfa27830c94fa5323ea5023073387a355b24ed1
Logan Rakai <lrakai@users.noreply.github.com>
2018-04-26 13:27:29 -0600

    Exclude .git directory from S3 package

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -rf src.zip
-zip -r src.zip . -x build/\* node_modules/\* public/\* .gitignore package-lock.json package.sh
+zip -r src.zip . -x build/\* node_modules/\* public/\* .git/\* .gitignore package-lock.json package.sh
